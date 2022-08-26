666435ce271924d8d9e2497bbe249d0ae3736e7c
manhkkkk <pktopad@gmail.com>
2022-08-26 15:34:17 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const middlewares = jsonServer.defaults({
   static: './build'
 });
 const PORT = process.env.PORT || 4000;
+server.use(cors())
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
